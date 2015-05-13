d38a000770803ed4008ceb4d7440f8310d8ac606
Punit Jajodia <punit@parewalabs.com>
2015-05-13 15:50:25 +0545

    Autorefresh csv created manually from API

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var tabletop = require('tabletop');
 var json2csv = require('json2csv');
+var request = require('request');
 
 router.get('/csv', function(req, res, next) {
 	tabletop.init( { key: '1FlFzSqdaQp9lEv4rALC6dND0JxJoDBFAyNE5K-1zdQc',
@@ -16,6 +17,52 @@ router.get('/csv', function(req, res, next) {
                    simpleSheet: true } )
   // res.render('index', { title: 'Express' });
 });
+
+var quakemap = {};
+quakemap.readableIncidentMode = function(incidentModeCode){
+	switch(incidentModeCode){
+		case "1":
+			return "WEB";
+		case "2":
+			return "SMS";
+		case "3":
+			return "EMAIL";
+		case "4":
+			return "TWITTER";
+	}
+};
+
+router.get('/autorefresh', function(req, res, next){
+	console.log(quakemap.readableIncidentMode(1));
+	
+	request('http://quakemap.org/api?task=incidents', function (error, response, body) {
+	    if (!error && response.statusCode == 200) {
+	    	var body = JSON.parse(body);
+	   
+	    	var incidents = body.payload.incidents;
+	    	var columns = [];
+	    	var rows = [];
+
+			 for(var i=0; i<incidents.length; i++){
+			 	var incident = incidents[i].incident;
+			 	columns.push(incident.incidentid);
+			 	columns.push(incident.incidenttitle);
+				columns.push(incident.incidentdescription);
+				columns.push(incident.incidentdate);
+				columns.push(quakemap.readableIncidentMode(incident.incidentmode));
+				columns.push(incident.incidentactive?"YES":"NO");
+				columns.push(incident.incidentverified?"YES":"NO");
+				columns.push(incident.locationname);
+				columns.push(incident.locationlatitude);
+				columns.push(incident.locationlongitude);
+				rows.push(columns.join(";"));
+				columns = [];
+	    	}
+	    	res.send(rows.join("<br/>"));
+	    }
+	});
+
+}); 
 
 router.get('/', function(req, res, next) {
    res.render('index', { title: 'Quakemap' });
