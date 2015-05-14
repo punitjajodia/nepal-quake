ee57e710951b0b51d05c6e46476fe0c7dd2753e3
Punit Jajodia <punit@parewalabs.com>
2015-05-14 23:03:21 +0545

    Changed path of csv to absolute so that cron job doesn't fail because of relative path issues

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -1,1 +1,1 @@
-wget "http://quakemap.org/index.php/export_reports/index/csv/" -O "../public/quakemap-data.csv" && wget "localhost:3000/refresh" -O /dev/null 
+wget "http://quakemap.org/index.php/export_reports/index/csv/" -O "/var/www/nepal-quake/public/quakemap-data.csv" && wget "localhost:3000/refresh" -O /dev/null 
