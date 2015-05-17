8d7090b54a2113c587f9d56eb7192d8919d4acaf
Punit Jajodia <punit@parewalabs.com>
2015-05-17 22:53:48 +0545

    Made filter urls shorter

--- public/js/quakemap-print.js
+++ public/js/quakemap-print.js
@@ -1,1 +1,18 @@
+$(function()
+{
+
+
 $( "#accordion" ).accordion({collapsible: true, active: false});
+
+$("form").submit(function()
+    {
+        $(this).children('select').each(function(){
+        	console.log($(this).val());
+        	 if($(this).val() == "Click to apply filter") {
+        	 	$(this).attr("disabled", "disabled");
+        	 }
+        });
+        return true; // ensure form still submits
+    });
+
+});
